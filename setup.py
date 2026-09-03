f84ae57390e37fd855438af6512398bf1bd0bb0a
victor <creategitpod@gmail.com>
2026-09-03 02:37:25 +0100

    error handling pipeline

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="montra_analysis_system",
+    author = "victor",
+    author_email="oluikpe4real@yahoo.com",
+    version = '1.0.0'
+)
---
+++ src/logger/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_DIRS = "logs"
+os.makedirs(LOG_DIRS, exist_ok=True)
+
+def configure_logging():
+    logger = logging.getLogger()
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        "[%(asctime)s %(name)s - %(levelname)s - %(message)s]"
+    ) 
+
+    # creating a new file for every run
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    log_file = os.path.join(LOG_DIRS, f"run_{timestamp}.log")
+
+    # filehandler it handle the streaming process of log in a specific file
+    file_handler = logging.FileHandler(
+        log_file, encoding="utf-8"
+
+    )
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    # console handler
+    console_handler = logging.streamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
